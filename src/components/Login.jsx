5519926a77e761fbe7dc116c7a459b1f5a408100
Charles Watson <charles@charless-mbp-2.lan>
2018-06-20 23:31:41 -0400

    add login box and effects

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,11 +3,33 @@ import React, { Component } from "react";
 class Login extends Component {
   constructor() {
     super();
+    this.state = {
+      displayLogin: false
+    };
   }
+
+  loginInit = () => {
+    this.setState({ displayLogin: true });
+  };
+
   render() {
     return (
       <div>
-        <h1>Component Test</h1>
+        <h3
+          id="login-trigger"
+          style={this.state.displayLogin ? { display: "none" } : {}}
+          onClick={this.loginInit}>
+          Login
+        </h3>
+        <h1
+          id={this.state.displayLogin ? "login-header-slide" : "login-header"}>
+          Welcome
+        </h1>
+
+        {/* LOGIN BOX */}
+        <div id={this.state.displayLogin ? "displayLogin" : "hideLogin"}>
+          <h4>Login to access endless high fidelity images</h4>
+        </div>
       </div>
     );
   }
